2db3631f84c322656e4fd77650c22f1d7d36cfc1
Siegmeyer0of0Catarina <siegmeyerofcatarina0x@gmail.com>
2024-05-02 10:38:35 +0300

    Lb5
    System testing

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.tBpath = new System.Windows.Forms.TextBox();
+            this.btOpenFile = new System.Windows.Forms.Button();
+            this.lBdata = new System.Windows.Forms.ListBox();
+            this.FilterButton = new System.Windows.Forms.Button();
+            this.WriteButton = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tBpath
+            // 
+            this.tBpath.Location = new System.Drawing.Point(12, 12);
+            this.tBpath.Name = "tBpath";
+            this.tBpath.ReadOnly = true;
+            this.tBpath.Size = new System.Drawing.Size(388, 20);
+            this.tBpath.TabIndex = 0;
+            // 
+            // btOpenFile
+            // 
+            this.btOpenFile.Location = new System.Drawing.Point(406, 10);
+            this.btOpenFile.Name = "btOpenFile";
+            this.btOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.btOpenFile.TabIndex = 1;
+            this.btOpenFile.Text = "Открыть";
+            this.btOpenFile.UseVisualStyleBackColor = true;
+            this.btOpenFile.Click += new System.EventHandler(this.btOpenFile_Click);
+            // 
+            // lBdata
+            // 
+            this.lBdata.FormattingEnabled = true;
+            this.lBdata.Location = new System.Drawing.Point(12, 38);
+            this.lBdata.Name = "lBdata";
+            this.lBdata.Size = new System.Drawing.Size(469, 277);
+            this.lBdata.TabIndex = 2;
+            // 
+            // FilterButton
+            // 
+            this.FilterButton.Enabled = false;
+            this.FilterButton.Location = new System.Drawing.Point(12, 321);
+            this.FilterButton.Name = "FilterButton";
+            this.FilterButton.Size = new System.Drawing.Size(230, 23);
+            this.FilterButton.TabIndex = 3;
+            this.FilterButton.Text = "Фильтрация";
+            this.FilterButton.UseVisualStyleBackColor = true;
+            this.FilterButton.Click += new System.EventHandler(this.FilterButton_Click);
+            // 
+            // WriteButton
+            // 
+            this.WriteButton.Enabled = false;
+            this.WriteButton.Location = new System.Drawing.Point(251, 321);
+            this.WriteButton.Name = "WriteButton";
+            this.WriteButton.Size = new System.Drawing.Size(230, 23);
+            this.WriteButton.TabIndex = 4;
+            this.WriteButton.Text = "Построение графика";
+            this.WriteButton.UseVisualStyleBackColor = true;
+            this.WriteButton.Click += new System.EventHandler(this.WriteButton_Click);
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(12, 350);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(469, 215);
+            this.chart1.TabIndex = 5;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 577);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.WriteButton);
+            this.Controls.Add(this.FilterButton);
+            this.Controls.Add(this.lBdata);
+            this.Controls.Add(this.btOpenFile);
+            this.Controls.Add(this.tBpath);
+            this.Name = "Form1";
+            this.Text = "Лб5";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tBpath;
+        private System.Windows.Forms.Button btOpenFile;
+        private System.Windows.Forms.ListBox lBdata;
+        private System.Windows.Forms.Button FilterButton;
+        private System.Windows.Forms.Button WriteButton;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // Установка свойства FormBorderStyle
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            // Запрет разворачивания формы на весь экран
+            this.MaximizeBox = false;
+        }
+
+        private void btOpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                tBpath.Text = filePath;
+
+                // Очистим ListBox перед новым чтением файла
+                lBdata.Items.Clear();
+
+                // Прочитаем данные из файла и добавим каждую строку в ListBox
+                try
+                {
+                    using (StreamReader reader = new StreamReader(filePath))
+                    {
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            lBdata.Items.Add(line);
+                        }
+                    }
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show("Ошибка при чтении файла: " + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            FilterButton.Enabled = true;
+        }
+
+        private void FilterButton_Click(object sender, EventArgs e)
+        {
+            // Проверим, что есть данные в ListBox
+            if (lBdata.Items.Count == 0)
+            {
+                MessageBox.Show("No data to filter. Please load a file first.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            List<string> lines = new List<string>();            
+            // Пройдемся по каждой строке в ListBox
+            foreach (string line in lBdata.Items)
+            {
+                string[] columns = null;
+                if(line.Contains("\t"))
+                columns =  line.Split('\t');
+                else columns = line.Split(',');
+
+                string newLine = columns[0] + "," + columns[1] + "," + columns[3]
+                    + "," + columns[4] + "," + columns[5] + "," + columns[6] + "," + columns[12];
+
+                lines.Add(newLine);                  
+            }
+
+            lBdata.Items.Clear();
+
+            foreach(string s in lines)
+            {
+                lBdata.Items.Add(s);
+            }
+            FilterButton.Enabled = false;
+            WriteButton.Enabled = true;
+        }
+
+        private void WriteButton_Click(object sender, EventArgs e)
+        {
+            chart1.Series.Clear();
+            Series series1 = new Series();
+            series1.Name = "Indoor Humidity";
+            Series series2 = new Series();
+            series2.Name = "Indoor Temperature";
+            Series series3 = new Series();
+            series3.Name = "Outdoor Humidity";
+            Series series4 = new Series();
+            series4.Name = "Outdoor Temperature";
+            Series series5 = new Series();
+            series5.Name = "Dewpoint";
+
+            int i = 0;
+            foreach (string line in lBdata.Items)
+            {
+                if (i > 0)
+                {
+                    string[] columns = line.Split(',');
+
+                    string[] time = null;
+                    if (columns[1].Contains(" "))
+                        time = columns[1].Split(' ');
+
+                    series1.Points.Add(new DataPoint(ConvertToMinutes(time[1]), columns[2]));
+                    DataPoint point = new DataPoint(ConvertToMinutes(time[1]), RemoveDecimalPart(columns[3]));
+                    series2.Points.Add(point);
+                    point = new DataPoint(ConvertToMinutes(time[1]), RemoveDecimalPart(columns[4]));
+                    series3.Points.Add(point);
+                    point = new DataPoint(ConvertToMinutes(time[1]), RemoveDecimalPart(columns[5]));
+                    series4.Points.Add(point);
+                    point = new DataPoint(ConvertToMinutes(time[1]), RemoveDecimalPart(columns[6]));
+                    series5.Points.Add(point);
+                }
+                i++;
+            }
+            series1.ChartType = SeriesChartType.Line;
+            series2.ChartType = SeriesChartType.Line;
+            series3.ChartType = SeriesChartType.Line;
+            series4.ChartType = SeriesChartType.Line;
+            series5.ChartType = SeriesChartType.Line;
+            chart1.Series.Add(series1);
+            chart1.Series.Add(series2);
+            chart1.Series.Add(series3);
+            chart1.Series.Add(series4);
+            chart1.Series.Add(series5);
+            
+            WriteButton.Enabled = false;
+        }
+
+        static double ConvertToMinutes(string timeString)
+        {
+            // Разбиваем строку времени на часы и минуты
+            string[] timeParts = timeString.Split(':');
+
+            // Парсим часы и минуты из строкового представления в числа
+            if (timeParts.Length == 2 && int.TryParse(timeParts[0], out int hours) && int.TryParse(timeParts[1], out int minutes))
+            {
+                // Преобразуем часы в минуты и складываем с минутами
+                return hours * 60 + minutes;
+            }
+            else
+            {
+                // В случае ошибки ввода возвращаем значение, которое можно обработать
+                return -1;
+            }
+        }
+
+        static string RemoveDecimalPart(string input)
+        {
+            // Находим индекс точки в строке
+            int dotIndex = input.IndexOf('.');
+
+            // Если точка не найдена, возвращаем исходную строку
+            if (dotIndex == -1)
+            {
+                return input;
+            }
+
+            // Обрезаем строку до точки, включительно
+            string result = input.Substring(0, dotIndex + 1);
+
+            return result;
+        }
+
+
+    }
+
+
+}
